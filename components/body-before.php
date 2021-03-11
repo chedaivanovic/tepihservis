46c7721c62a7dc39800dd5e8609e07f6373039f6
Čedomir Ivanović <chedaivanovic@gmail.com>
2021-03-12 00:28:20 +0100

    Initial Commit

---
+++ components/body-before.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="main-header">
+        <div class="d-flex justify-content-between">
+            <ul id="header-phones">
+                <li><a href="tel:"></a></li>
+                <li><a href="tel:"></a></li>
+            </ul>
+            <ul class="social-links">
+                <li>
+                    <a target="_blank" href="">
+                        <i class="fas fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="">
+                        <i class="fas fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php $title = 'S&J Cleaning Company'; ?>
+<?php include('components/header') ?>
+<?php include('components/body-before') ?>
+
+
+
+
+<?php include('components/body-after') ?>
+<?php include('components/footer') ?>
