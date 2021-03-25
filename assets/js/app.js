0a9cb72bf5df277ae9047767619e0fc3f0ddd7af
Čedomir Ivanović <chedaivanovic@gmail.com>
2021-03-26 00:23:44 +0100

    Animation added, responsive started, added mixins and email.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,53 @@
 $('.hero-slider').slick({
-    dots:true
+  dots: true
 });
 $('.users-counter-slider').slick({
-    slidesToShow: 3
+  slidesToShow: 3
 });
 
+
+
 var sessionCount = 0;
-$(window).on('scroll',function() {
+
+var headerMain = $(".main-header")[0];
+var headerSec = $(".top-header")[0];
+var body = $('body')[0];
+console.log(headerSec);
+$(window).on('scroll', function () {
 
   var oTop = $('#counter').offset().top - window.innerHeight;
   if (sessionCount == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
+    setTimeout(function () {
+      $('.counter-value').each(function () {
+        var $this = $(this),
+          countTo = $this.attr('data-count');
+        $({
+          countNum: $this.text()
+        }).animate({
           countNum: countTo
         },
-        {
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-          }
-        });
-    });
+          {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            }
+          });
+      });
+    }, 500);
     sessionCount = 1;
+  };
+  if (window.pageYOffset > 40) {
+    $(headerMain).addClass("sticky");
+    $(body).addClass('header-sticky');
+  } else {
+    $(headerMain).removeClass("sticky");
+    $(body).removeClass('header-sticky');
   }
 });
-
-window.onscroll = function() {myFunction()};
-
-var headerMain = document.getElementsByClassName("main-header")[0];
-var headerSec = document.getElementsByClassName("sec-header")[0];
-var body = document.getElementsByTagName('body')[0];
-function myFunction() {
-  if (window.pageYOffset > 40) {
-    headerMain.classList.add("sticky");
-    body.classList.add('header-sticky')
-  } else {
-    headerMain.classList.remove("sticky");
-    body.classList.remove('header-sticky')
-  }
-}
+AOS.init({
+  duration: 1200,
+});
--- components/body-before.php
+++ components/body-before.php
@@ -1,12 +1,12 @@
 <header>
     <div class="top-header d-flex justify-content-between">
         <div class="wrapper">
-            <div class="d-flex justify-content-between w-100">
+            <div class="d-flex justify-content-between w-100 flex-wrap">
                 <ul id="header-phones" class="d-flex">
-                    <li><a href="tel:+381658700038">065/87-000-38</a></li>
-                    <li><a href="tel:+0649399843">064/939-98-43</a></li>
+                    <li><a href="tel:+381658700038"><span class="d-block d-sm-none"><i class="fas fa-phone"></i> 1</span><span class="d-none d-sm-block">065/87-000-38</span></a></li>
+                    <li><a href="tel:+0649399843"><span class="d-block d-sm-none"><i class="fas fa-phone"></i> 2</span><span class="d-none d-sm-block">064/939-98-43</span></a></li>
                 </ul>
-                <!-- <ul class="social-links d-flex">
+                <ul class="social-links d-flex d-md-none">
                     <li>
                         <a target="_blank" class="fb-icon" href="">
                             <i class="fab fa-facebook-f"></i>
@@ -17,8 +17,8 @@
                             <i class="fab fa-instagram"></i>
                         </a>
                     </li>
-                </ul> -->
-                <h1>S&J | Tepih Servis | Dubinsko pranje | Beograd</h1>
+                </ul>
+                <h1 class="text-center text-md-left">S&J | Tepih Servis | Dubinsko pranje | Beograd</h1>
             </div>
         </div>
     </div>
