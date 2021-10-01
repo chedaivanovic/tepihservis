0550d96ef80a004d7b4554a49c0bbf5bf2fde7c4
Čedomir Ivanović <chedaivanovic@gmail.com>
2021-10-02 01:03:34 +0200

    Changed names of files to .min. to indicate minifed

--- components/footer.php
+++ components/footer.php
@@ -16,6 +16,6 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/popper.min.js" integrity="sha512-eHo1pysFqNmmGhQ8DnYZfBVDlgFSbv3rxS0b/5+Eyvgem/xk0068cceD8GTlJOZsUrtjANIrFhhlwmsL1K3PKg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-<script src="assets/js/app.js?v=1.0.1"></script>
+<script src="assets/js/app.min.js?v=1.0.1"></script>
 </body>
 </html>
